045cf8e2071e36b7f42d5fa3628d1da1792d84fc
alexeyklykov <d1stur8ed@ukr.net>
2017-02-03 15:17:06 +0200

    continue hw16

--- hw16/app.js
+++ hw16/app.js
@@ -1,32 +1,72 @@
 angular.module("myApp",[])
 	.controller("firstCtrl", function($scope, $http){
-		//Создаем временную переменную
-		//$scope.tempInput = "тестовое";
-		//создаем хранилище для всех заданий
-		var myApp = angular.module('myApp', []);
-		myApp.controller('firstCtrl', function ($scope, $http){
-			$http.get('file.json').success(function(data) {
-				$scope.tasksArr = data;
-			});
-		});
+		//Временная переменная
+		//$scope.tempInput = "test";
+		//Хранилище для всех заданий
+		/*$scope.tasksArr = [
+			{ 
+				"text": "Chocolate",
+				"done": "true"
+			},
+			{
+				"text": "Potato",
+				"done": false 
+			},
+			{ 
+				"text": "Banana",
+				"done": false 
+			},
+			{ 
+				"text": "Water", 
+				"done": true 
+			}
+		];*/
+		//Писать массив в localStorage
+		$scope.tasksArr = JSON.parse(localStorage.getItem('tasksArr')) || []
+
+		//Перенести из временного хранилища в общие задания
+		$scope.addTask = function(){
+			$scope.tasksArr.unshift({ text: $scope.tempInput, done: false });
+			$scope.tempInput = "";
+		}
+		//Удалить пункт
+		$scope.deleteTask = function(){
+			$scope.tasksArr = $scope.tasksArr.filter(function(item){
+				return !item.done
+			})
+		}
+
+		//Изменить пункт
+		$scope.toggleEditMode = function(){
+       $(event.target).closest('li').toggleClass('editing');
+    };
+  	$scope.editOnEnter = function(item){
+      if(event.keyCode == 13 && item.text){
+          $scope.toggleEditMode();
+      }
+  	};
+
+  	//Изменение данных
+		$scope.$watch('tasksArr',function(newVal,oldVal){
+			if(newVal!=oldVal){
+				localStorage.setItem('tasksArr',JSON.stringify(newVal))
+			}
+			},true)
+
+		//Счетчик остатка
+		$scope.remain = function () {
+        var count = $scope.tasksArr.length;
+        angular.forEach($scope.tasksArr, function(item) {
+        	var sObj = JSON.stringify($scope.tasksArr);
+					localStorage.setItem("tasksArr", sObj);
+          count -= item.done;
+        });
+        return count;
+    };
+
+
+
+
 });
 
-/*
-		//Создаем функцию, которая переносит из временного хранилища в общие задания
-		$scope.addNew = function(){
-			if($scope.tempInput){
-				$scope.tasksArr.push($scope.tempInput);
-				$scope.tempInput = "";
-			} else {
-				alert("Нет задания!");
-			}
-		}
 
-		$scope.deleteItem = function(item){
-			var index = $scope.tasksArr.indexOf(item);
-			$scope.tasksArr.splice(index, 1);
-		}
-
-
-
-});*/
